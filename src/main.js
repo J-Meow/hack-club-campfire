ead4734294d3734bd53a5706b50b0c5795f2d7f1
Jmeow <j@jmeow.net>
2026-02-28 13:31:44 -0600

    Player/jumping

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ const ctx = canvas.getContext("2d")
 const width = 1600
 const height = 900
 let objects = [{ type: "spike", x: 1000, y: 0, width: 50, height: 50 }]
-let speed = 0.6
+let player = { x: 50, y: 0, width: 50, height: 50, yVel: 0 }
+let speed = 0.8
+let gravity = 0.01
 function draw() {
     ctx.save()
     ctx.scale(2, 2)
@@ -33,17 +35,52 @@ function draw() {
         }
     })
 
+    ctx.fillStyle = "blue"
+    ctx.fillRect(
+        player.x,
+        height - 300 - player.y - player.height,
+        player.width,
+        player.height,
+    )
+
     ctx.restore()
     requestAnimationFrame(draw)
 }
 let lastUpdate = Date.now()
+let keys = []
 function update() {
     const delta = -lastUpdate + (lastUpdate = Date.now())
     objects.forEach((obj) => {
         obj.x -= speed * delta
     })
     objects = objects.filter((obj) => obj.x > -obj.width - 100)
+    if (
+        keys.includes("ArrowUp") ||
+        keys.includes("KeyW") ||
+        keys.includes("Space")
+    ) {
+        if (player.y == 0) {
+            player.yVel = 2
+        }
+    }
+    player.y += player.yVel * delta
+    player.yVel -= gravity * delta
+    if (player.y <= 0) {
+        player.yVel = 0
+        player.y = 0
+    }
     setTimeout(update, 1000 / 60)
 }
 draw()
 update()
+addEventListener("keydown", (ev) => {
+    if (keys.includes(ev.code)) return
+    keys.push(ev.code)
+})
+addEventListener("keyup", (ev) => {
+    if (!keys.includes(ev.code)) return
+    keys.splice(keys.indexOf(ev.code), 1)
+})
+addEventListener("blur", (ev) => {
+    keys = []
+})
