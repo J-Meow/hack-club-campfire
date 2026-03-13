7c7d734ae542f5cba4d08ddc5b6797222498309c
Jmeow <j@jmeow.net>
2026-03-13 13:36:50 -0500

    leaderboard for v2

--- leaderboard.js
+++ leaderboard.js
@@ -32,6 +32,37 @@ const server = Bun.serve({
                 return res
             },
         },
+        "/leaderboard/v2": {
+            GET: async () => {
+                const rows =
+                    await sql`SELECT score, initials FROM leaderboardv2 ORDER BY score DESC FETCH FIRST 10 ROWS ONLY`
+                let res = Response.json(rows, {
+                    headers: { "Access-Control-Allow-Origin": "*" },
+                })
+                res.headers.set("Access-Control-Allow-Origin", "*")
+                res.headers.set(
+                    "Access-Control-Allow-Methods",
+                    "GET, POST, PUT, DELETE, OPTIONS",
+                )
+                return res
+            },
+            POST: async (req) => {
+                const body = await req.json()
+                await sql`INSERT INTO leaderboardv2("score", "initials") VALUES(${body.score}, ${body.initials})`
+                let res = Response.json(
+                    {},
+                    {
+                        headers: { "Access-Control-Allow-Origin": "*" },
+                    },
+                )
+                res.headers.set("Access-Control-Allow-Origin", "*")
+                res.headers.set(
+                    "Access-Control-Allow-Methods",
+                    "GET, POST, PUT, DELETE, OPTIONS",
+                )
+                return res
+            },
+        },
     },
     port: 9271,
 })
